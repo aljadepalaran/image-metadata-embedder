5bb7a7128ddee1186cdc69cc682bd251c4e31b04
Al Jade Palaran <aljadepalaran@Als-MacBook-Air.local>
2025-02-27 14:45:21 +0000

    display image metadata

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
-from PIL import Image
+from PIL import Image, ExifTags
 print("Running...")
 
 if __name__ == "__main__":
-  with Image.open("./test.jpg") as im:
-    print("test")
-    print(im.size)
+  with Image.open("./test.jpg") as img:
+    exif = { ExifTags.TAGS[k]: v for k, v in img._getexif().items() if k in ExifTags.TAGS }
+    print(f"Lens: {exif['LensModel']}")
+    print(f"FocalLength: {exif['FocalLength']}mm")
+    exposure_time = float(exif['ExposureTime'])
+    shutter_speed = f"1/{1 / exposure_time}"
+    print(f"Shutter Speed: {shutter_speed}")
+    print(f"Aperature: f{exif['FNumber']}")
+    print(img.size)
