0f77040a133e02d0ee354f2032354b27e938b816
Al Jade Palaran <aljadepalaran@Als-MacBook-Air.local>
2025-03-01 23:22:13 +0000

    add check for no exif data

--- image_embedder.py
+++ image_embedder.py
@@ -5,16 +5,20 @@ import os
 class ImageEmbedder:
   def process_image(source, opts):
     image = Image.open(source)
-    exif = { ExifTags.TAGS[k]: v for k, v in image._getexif().items() if k in ExifTags.TAGS }
-    focal_length, exposure_time, aperature = parse_exif(exif)
-    space = int(opts['space']) * ' '
-    text_to_add = f"{focal_length}mm{space}1/{int(1 / exposure_time)}{space}f/{aperature}"
-    position = tuple(map(int, opts['pos'].split(',')))
-    size = int(opts['size'])
-    output = add_text(image, text_to_add, position, size, (255, 255, 255))
-    output_filename = generate_filename(source)
-    output.save(output_filename)
-    print(f'Process complete. {output_filename} created.')
+    image_exif = image._getexif()
+    if type(image_exif) == type(None):
+      print(f'Skipping {source}, no exif data.')
+    else:
+      exif = { ExifTags.TAGS[k]: v for k, v in image_exif.items() if k in ExifTags.TAGS }
+      focal_length, exposure_time, aperature = parse_exif(exif)
+      space = int(opts['space']) * ' '
+      text_to_add = f"{focal_length}mm{space}1/{int(1 / exposure_time)}{space}f/{aperature}"
+      position = tuple(map(int, opts['pos'].split(',')))
+      size = int(opts['size'])
+      output = add_text(image, text_to_add, position, size, (255, 255, 255))
+      output_filename = generate_filename(source)
+      output.save(output_filename)
+      print(f'Process complete. {output_filename} created.')
 
   @staticmethod
   def process_directory(source, recursive, opts):
